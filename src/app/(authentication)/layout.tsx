c79e008776e38647a3103bc68e3137eac5f0d8ce
Vijay3626 <vijaysekar3626@gmail.com>
2025-05-09 12:08:10 +0530

    login page.tsx

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -8,14 +8,14 @@ const layout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => {
 
 
         <div className="h-screen w-screen flex items-center justify-center p-5">
-            <div className={`w-full h-full flex  transition-transform duration-700 ease-in-out`}>
-                <div className={`bg-primary  hidden lg:block p-5 relative overflow-hidden w-2/5`}>
+            <div className={`w-full h-full flex  transition-transform duration-700 rounded-2xl ease-in-out`}>
+                <div className={`bg-primary  hidden lg:block p-5 relative rounded-l-2xl overflow-hidden w-2/5`}>
                     <h1 className='text-5xl font-sans text-white font-bold pt-5 pl-5'>Feel the rhythm<br />Live the music.</h1>
                     <div className="">
                         <Image src={login} alt="Tune Aura" width={0} height={0} className='absolute top-0 left-0 w-full h-full object-cover' />
                     </div>
                 </div>
-                <div className={`bg-[#F4F2FF]   transition-transform duration-700 ease-in-out overflow-hidden p-5 flex justify-center w-3/5`}>{children}</div>
+                <div className={`bg-[#F4F2FF] rounded-r-2xl transition-transform duration-700 ease-in-out overflow-hidden p-5 flex justify-center w-3/5`}>{children}</div>
             </div>
         </div>
     )
